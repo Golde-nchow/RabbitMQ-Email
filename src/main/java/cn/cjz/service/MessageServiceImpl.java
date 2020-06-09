47c7b922762ecc738e67bca2b8f89464fc28964a
Golden_Chow <570384231@qq.com>
2020-06-09 16:54:48 +0800

    添加发送邮件的业务类

---
+++ src/main/java/cn/cjz/service/MessageServiceImpl.java
@@ -0,0 +1,46 @@
+package cn.cjz.service;
+
+import cn.cjz.dao.MessageMapper;
+import cn.cjz.model.Mail;
+import cn.cjz.model.Message;
+import cn.cjz.model.MessageResponse;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.json.JSONUtil;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/6/9 15:51
+ * @description: 消息业务层
+ * @version: 1.0
+ */
+@Service
+public class MessageServiceImpl {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    public String send(Mail mail) {
+        String msgId = IdUtil.simpleUUID();
+        mail.setMsgId(msgId);
+
+        // 消息入库
+        Message message = Message
+                .builder()
+                .msgId(msgId)
+                .msgContent(JSONUtil.parse(mail).toString())
+                .msgExchange("")
+                .msgRoutingKey("")
+                .build();
+        messageMapper.insertSelective(message);
+
+        // 关联id
+        CorrelationData correlationData = new CorrelationData(msgId);
+
+        // 发送消息到rabbitMQ
+
+        return MessageResponse.success();
+    }
+}
