ddecfb36a73d3c1480d9fac34c9bc8fec2fb54e1
Golden_Chow <570384231@qq.com>
2020-06-09 16:53:52 +0800

    添加SpringBoot启动类

---
+++ src/main/java/cn/cjz/RabbitMqEmailApplication.java
@@ -0,0 +1,20 @@
+package cn.cjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/6/9 15:52
+ * @description: SpringBoot启动类
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class RabbitMqEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqEmailApplication.class, args);
+    }
+
+
+}
