0084b4d0c8a434f0f02d3949b122ae83f7aabf89
Golden_Chow <570384231@qq.com>
2020-06-10 20:47:16 +0800

    mapper：查询超时的邮件消息

--- src/main/java/cn/cjz/dao/MessageMapper.java
+++ src/main/java/cn/cjz/dao/MessageMapper.java
@@ -1,8 +1,11 @@
 package cn.cjz.dao;
 
 import cn.cjz.model.Message;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * @author: Kam-Chou
@@ -12,4 +15,11 @@ import tk.mybatis.mapper.common.Mapper;
  */
 @Component
 public interface MessageMapper extends Mapper<Message> {
+
+    /**
+     * 查询超时的邮件消息（由于失败导致1分钟后再重试）
+     */
+    @Select("SELECT * FROM msg_log WHERE msg_status = 0 AND now() >= next_retry_time")
+    List<Message> selectTimeoutMessage();
+
 }
