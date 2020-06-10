a979352224b491cc5fe12b065e9b9ad661f64943
Golden_Chow <570384231@qq.com>
2020-06-10 20:37:03 +0800

    为插入和修改操作添加时间

--- src/main/java/cn/cjz/service/MessageServiceImpl.java
+++ src/main/java/cn/cjz/service/MessageServiceImpl.java
@@ -6,12 +6,16 @@ import cn.cjz.model.Mail;
 import cn.cjz.model.Message;
 import cn.cjz.model.MessageResponse;
 import cn.cjz.mq.MessageHelper;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.json.JSONUtil;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @author: Kam-Chou
@@ -45,12 +49,20 @@ public class MessageServiceImpl {
         mail.setMsgId(msgId);
 
         // 消息入库
+        Date time = new Date();
+        DateTime createTime = new DateTime(time);
+        // 下次重试时间在失败的一分钟后
+        DateTime nextTryTime = DateUtil.offsetMinute(time, 1);
+
         Message message = Message
                 .builder()
                 .msgId(msgId)
                 .msgContent(JSONUtil.parse(mail).toString())
                 .msgExchange(RabbitConfig.MAIL_EXCHANGE)
                 .msgRoutingKey(RabbitConfig.MAIL_ROUTING_KEY)
+                .createTime(createTime.toSqlDate())
+                .nextRetryTime(nextTryTime.toSqlDate())
+                .updateTime(createTime.toSqlDate())
                 .build();
         messageMapper.insertSelective(message);
 
@@ -74,9 +86,13 @@ public class MessageServiceImpl {
      * @param status        消息状态
      */
     public void updateStatus(String correlationId, Integer status) {
+        Date time = new Date();
+        DateTime updateTime = new DateTime(time);
+
         Message message = Message.builder()
                 .msgId(correlationId)
                 .msgStatus(status)
+                .updateTime(updateTime.toSqlDate())
                 .build();
         messageMapper.updateByPrimaryKeySelective(message);
     }
