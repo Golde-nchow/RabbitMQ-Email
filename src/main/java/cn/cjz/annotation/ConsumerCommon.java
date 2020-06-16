165de03b069021af1e6edfc2e81ca02dd400cb00
Golden_Chow <570384231@qq.com>
2020-06-16 12:41:15 +0800

    创建消费者通用逻辑注解（幂等性、消费确认）

---
+++ src/main/java/cn/cjz/annotation/ConsumerCommon.java
@@ -0,0 +1,10 @@
+package cn.cjz.annotation;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/6/16 11:42
+ * @description: 消费者通用逻辑注解（幂等性、消费确认）
+ * @version: 1.0
+ */
+public @interface ConsumerCommon {
+}
